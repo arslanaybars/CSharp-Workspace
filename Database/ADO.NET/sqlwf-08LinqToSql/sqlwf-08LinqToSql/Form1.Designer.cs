466895924957f0bfa4b46b424c2ead7d42816ee4
Aybars Arslan <arslanaybars@gmail.com>
2015-10-14 00:22:34 +0300

    Codes Added

---
+++ Database/ADO.NET/sqlwf-08LinqToSql/sqlwf-08LinqToSql/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace sqlwf_08LinqToSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.dgvFilmler = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(178, 12);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(221, 23);
+            this.btnGetir.TabIndex = 0;
+            this.btnGetir.Text = "Filmleri Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // dgvFilmler
+            // 
+            this.dgvFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilmler.Location = new System.Drawing.Point(12, 41);
+            this.dgvFilmler.Name = "dgvFilmler";
+            this.dgvFilmler.Size = new System.Drawing.Size(554, 336);
+            this.dgvFilmler.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 389);
+            this.Controls.Add(this.dgvFilmler);
+            this.Controls.Add(this.btnGetir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.DataGridView dgvFilmler;
+    }
+}
+
---
+++ Database/ADO.NET/sqlwf-08LinqToSql/sqlwf-08LinqToSql/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sqlwf_08LinqToSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataClasses1DataContext dc = new DataClasses1DataContext();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            var Films = from film in dc.Filmlers
+                        where film.Varmi == true
+                        select new {film.FilmAd, film.Yonetmen, film.Oyuncular, film.Miktar};
+
+            dgvFilmler.DataSource = Films;
+        }
+    }
+}
