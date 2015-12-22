79dbfc95918456ac434ed9b7421b5fbdda03d902
Aybars Arslan <arslanaybars@gmail.com>
2015-12-23 00:52:00 +0200

    ViewBag usage

---
+++ Asp.Net MVC/mvc-04ViewBag/002MVCDemo/002MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _002MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //return "Id : " + id + " Name : " + Request.QueryString["name"];
+            //return "Id = " + id + " Name = " + name;
+
+            ViewBag.Countries =new List<string>
+            {
+                "Turkey",
+                "Lithuania",
+                "USA",
+                "Canada"
+            };
+
+            return View();
+
+        }
+
+        public string GetDetails()
+        {
+            return "GetDetails is working..";
+        }
+
+    }
+}
