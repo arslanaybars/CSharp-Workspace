74427b26db85446967afdd8ac65bcf0e1fe96aac
Aybars Arslan <arslanaybars@gmail.com>
2015-12-23 00:50:59 +0200

    ASP:NET MVC Genel Bakış

---
+++ Asp.Net MVC/mvc-03GenelBakis/Mvc4Application/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            // or youn see
+            // Refrences -> System.Web.Mvc -> properties -> check version
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+    }
+}
