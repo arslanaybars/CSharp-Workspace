2446886c8d3bc724c94e82a8edcc01a25fb604ad
Aybars Arslan <arslanaybars@gmail.com>
2015-12-24 23:40:18 +0200

    ViewBag usage

---
+++ Asp.Net MVC/mvc-05ViewData-ViewBag/mvc-05ViewData-ViewBag/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using mvc_05ViewData_ViewBag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_05ViewData_ViewBag.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeID = 101,
+                Name = "John",
+                Gender = "Male",
+                City = "London"
+            };
+
+            return View(employee);
+        }
+
+    }
+}
---
+++ Asp.Net MVC/mvc-05ViewData-ViewBag/mvc-05ViewData-ViewBag/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_05ViewData_ViewBag.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
