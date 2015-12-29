87f1562034c212d52a4167aa18cf77f34a1e7598
Aybars Arslan <arslanaybars@gmail.com>
2015-12-29 02:00:24 +0200

    Html Helpers, Action Link Usage

---
+++ Asp.Net MVC/mvc-07HtmlHelpers-ActionLink/mvc-05ViewData-ViewBag/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using mvc_05ViewData_ViewBag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_05ViewData_ViewBag.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+
+            //var employees = employeeContext.Employees.ToList()
+            List<Employee> employees = employeeContext.Employees.ToList();
+
+            return View(employees);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+}
---
+++ Asp.Net MVC/mvc-07HtmlHelpers-ActionLink/mvc-05ViewData-ViewBag/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc_05ViewData_ViewBag.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
