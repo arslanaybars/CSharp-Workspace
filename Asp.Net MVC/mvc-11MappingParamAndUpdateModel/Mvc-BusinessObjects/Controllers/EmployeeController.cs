85d84f82359ff9f4c5024851c254afb5161d29e5
Aybars Arslan <arslanaybars@gmail.com>
2016-01-04 01:31:59 +0200

    Understanding Mapping params and update model usage

---
+++ Asp.Net MVC/mvc-11MappingParamAndUpdateModel/Mvc-BusinessObjects/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace Mvc_BusinessObjects.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> emplooyes = employeeBusinessLayer.Employees.ToList();
+            return View(emplooyes);
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = new Employee();
+                UpdateModel<Employee>(employee);
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.AddEmployee(employee);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        /*
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.AddEmployee(employee);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+         * */
+
+        /*
+        [HttpPost]
+        public ActionResult Create(string name, string city, string gender, DateTime dateOfBirth)
+        {
+            Employee employee = new Employee();
+
+            employee.Name = name;
+            employee.Gender = gender;
+            employee.City = city;
+            employee.DateOfBirth = dateOfBirth;
+
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+
+            employeeBusinessLayer.AddEmployee(employee);
+            return RedirectToAction("Index");
+        }
+        */
+    }
+}
