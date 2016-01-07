c31647a08fab96f65764533ab9790da1671cf959
Aybars Arslan <arslanaybars@gmail.com>
2016-01-08 00:21:15 +0200

    Model Binding Using Interface

---
+++ Asp.Net MVC/mvc-14ModelBindingUsingInterface/BusinessLayer/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IEmployee
+    {
+        public int ID { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class Employee : IEmployee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
