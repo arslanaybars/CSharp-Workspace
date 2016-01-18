190094e4aaa4c1482ff62235b1339a1fecfd2eee
Aybars Arslan <arslanaybars@gmail.com>
2016-01-19 01:14:31 +0200

    Html Helper DropDownList

---
+++ Asp.Net MVC/mvc-20HtmlHelperDropDownList/Mvc_Demo/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using Mvc_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            /*
+            // Connect to the database
+            SampleDBContext db = new SampleDBContext();
+            // Retrieve departments, and build SelectList
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+
+            return View();
+             * */
+
+            SampleDBContext db = new SampleDBContext();
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+
+            foreach (Department department in db.Departments)
+            {
+                SelectListItem selectListItem = new SelectListItem
+                {
+                    Text = department.Name,
+                    Value = department.Id.ToString(),
+                    Selected = department.IsSelected.HasValue ? department.IsSelected.Value : false
+                };
+                selectListItems.Add(selectListItem);
+            }
+
+            ViewBag.Departments = selectListItems;
+            return View();
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
