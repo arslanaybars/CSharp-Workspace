0f860d8a7f5b27b2613d3e68e1046bcc782a5b19
Aybars Arslan <arslanaybars@gmail.com>
2016-01-24 18:06:04 +0200

    Introudction to ListBoxList

---
+++ Asp.Net MVC/mvc-23HtmlHelperListBoxList/MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Models;
+using System.Text;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            SampleDBContext db = new SampleDBContext();
+            List<SelectListItem> listSelectListItem = new List<SelectListItem>();
+
+            foreach (City city in db.Cities)
+            {
+                SelectListItem selectListItem = new SelectListItem()
+                {
+                    Text = city.Name,
+                    Value = city.ID.ToString(),
+                    Selected = city.IsSelected
+                };
+
+                listSelectListItem.Add(selectListItem);
+            }
+            CitiesViewModel citiesViewModel = new CitiesViewModel();
+            citiesViewModel.Cities = listSelectListItem;
+
+            return View(citiesViewModel);
+        }
+
+        [HttpPost]
+        public string Index(IEnumerable<string> selectedCities)
+        {
+            if(selectedCities == null)
+            {
+                return "You did not select any city";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("You Seleceted - " + string.Join(",", selectedCities));
+                return sb.ToString();
+            }
+
+        }
+    }
+}
---
+++ Asp.Net MVC/mvc-23HtmlHelperListBoxList/MvcDemo/Models/CitiesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Models
+{
+    public class CitiesViewModel
+    {
+        public IEnumerable<SelectListItem> Cities { get; set; }
+        public IEnumerable<string> SelectedCities { get; set; }
+    }
+}
