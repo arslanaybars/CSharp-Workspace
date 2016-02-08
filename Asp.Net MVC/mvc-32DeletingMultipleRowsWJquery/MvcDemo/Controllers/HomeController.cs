e737b8ba8a2b21a609d8cd495bc106d791f84329
Aybars Arslan <arslanaybars@gmail.com>
2016-02-09 01:34:50 +0200

    Deleting multiple rows with jquery

---
+++ Asp.Net MVC/mvc-32DeletingMultipleRowsWJquery/MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Models;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        SampleDBContext db = new SampleDBContext();
+        
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //List<Employee> Employee = db.Employee.ToList();
+            return View(db.Employee.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(IEnumerable<int> employeeIdsToDelete)
+        {
+            List<Employee> employees = db.Employee.Where(x => employeeIdsToDelete.Contains(x.ID)).ToList();
+
+            foreach (Employee employee in employees)
+            {
+                db.Employee.Remove(employee);
+            }
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
